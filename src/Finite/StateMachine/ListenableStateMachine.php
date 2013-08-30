091dc6a31bf716db1bfd3042467f58a3d981a166
yohang <yohan@giarel.li>
2013-08-30 15:06:41 +0200

    Merged StateMachine and ListenableStateMachine classes

--- src/Finite/StateMachine/ListenableStateMachine.php
+++ src/Finite/StateMachine/ListenableStateMachine.php
@@ -2,58 +2,15 @@
 
 namespace Finite\StateMachine;
 
-use Finite\Event\FiniteEvents;
-use Finite\Event\StateMachineEvent;
-use Finite\Event\TransitionEvent;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-
 /**
  * An Event-aware State machine.
  *
  * Uses the Symfony EventDispatcher Component
  *
+ * @deprecated Deprecated since version 1.0.0-BETA2. Use the Basic State Machine (which is now listenable) instead.
+ *
  * @author Yohan Giarelli <yohan@frequence-web.fr>
  */
 class ListenableStateMachine extends StateMachine
 {
-    /**
-     * @var EventDispatcherInterface
-     */
-    protected $dispatcher;
-
-    /**
-     * @param EventDispatcherInterface $dispatcher
-     */
-    public function setEventDispatcher(EventDispatcherInterface $dispatcher)
-    {
-        $this->dispatcher = $dispatcher;
-    }
-
-    /**
-     * @{inheritDoc}
-     */
-    public function initialize()
-    {
-        parent::initialize();
-        $this->dispatcher->dispatch(FiniteEvents::INITIALIZE, new StateMachineEvent($this));
-    }
-
-    /**
-     * @{inheritDoc}
-     */
-    public function apply($transitionName)
-    {
-        $transition = $this->getTransition($transitionName);
-        $event      = new TransitionEvent($this->getCurrentState(), $transition, $this);
-
-        $this->dispatcher->dispatch(FiniteEvents::PRE_TRANSITION, $event);
-        $this->dispatcher->dispatch(FiniteEvents::PRE_TRANSITION.'.'.$transitionName, $event);
-
-        $value = parent::apply($transitionName);
-
-        $this->dispatcher->dispatch(FiniteEvents::POST_TRANSITION, $event);
-        $this->dispatcher->dispatch(FiniteEvents::POST_TRANSITION.'.'.$transitionName, $event);
-
-        return $value;
-    }
 }
